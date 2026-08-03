b0ab41a5de2b4f0f2f855b6b112d0a6690fcd3a6
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-08-03 23:43:36 +0530

    added

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -107,8 +107,8 @@ resource "aws_security_group_rule" "rabbitmq_payment" {
 
 resource "aws_security_group_rule" "rabbitmq-bastion" {
     type = "ingress"
-    from_port = 3306
-    to_port = 3306
+    from_port = 22
+    to_port = 22
     protocol = "tcp"
     source_security_group_id = local.bastion_sg_id
     security_group_id = local.rabbitmq_sg_id
