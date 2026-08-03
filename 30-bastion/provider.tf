9908f66087ad005cf216d61f47a8076711b1ea71
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-08-03 22:19:47 +0530

    added

---
+++ 30-bastion/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "bmkrishtech-vpc-state"
+    key = "bmkrishtech-bastion"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+  
+}
---
+++ 40-databases/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+component=$1 #mongodb
+environment=$2 #dev
+dnf install ansible -y
+mkdir -p /var/log/roboshop/
+chown -R ec2-user:ec2-user /var/log/roboshop
+chmod -R 755 /var/log/roboshop
+touch /var/log/roboshop/ansible.log
+
+cd /home/ec2-user
+git clone https://github.com/muddukrishna1505/roboshop-ansible-version3.git
+cd roboshop-ansible-version3
+git pull
+ansible-playbook -e component=$component -e env=$environment roboshop.yaml
