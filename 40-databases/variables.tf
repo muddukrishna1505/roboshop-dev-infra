b5e70b8583b27391799ef4d14f3ed59c2977dbf4
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-08-03 23:02:42 +0530

    added

---
+++ 40-databases/variables.tf
@@ -0,0 +1,21 @@
+variable "project" {
+    default = "roboshop"
+  
+}
+
+variable "environment" {
+    default = "dev"
+  
+}
+
+variable "zone_id" {
+    default = "Z00981222UH92I1EWAVNF"
+}
+
+variable "domain_name" {
+    default = "bmkrishtech.online"
+}
+
+variable "mysql_root_password" {
+    type = string
+}
