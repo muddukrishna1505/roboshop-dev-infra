902338a51f00b8740c090e8f8e3e25a32855b8af
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-08-13 00:03:11 +0530

    added

---
+++ 90-components/variables.tf
@@ -0,0 +1,40 @@
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z00981222UH92I1EWAVNF"
+}  
+
+variable "domain_name" {
+    default = "bmkrishtech.online"
+}
+
+variable "components" {
+    default = {
+        catalogue = {
+            rule_priority = 10
+            app_version = "v3"
+        }
+        user = {
+            rule_priority = 20
+            app_version = "v3"
+        }
+        cart = {
+            rule_priority = 30
+            app_version = "v3"
+        }
+        shipping = {
+            rule_priority = 40
+            app_version = "v3"
+        }
+        payment = {
+            rule_priority = 50
+            app_version = "v3"
+        }
+        frontend = {
+            rule_priority = 10
+            app_version = "v3"
+        }
+    }
+}
